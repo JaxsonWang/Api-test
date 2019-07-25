aedc6d0bdc2f44058a808e694a3e20fa5bba3571
淮城一只猫 <i@iiong.com>
2019-07-25 10:54:59 +0800

    更新项目源码

---
+++ plugins/hapi-good.js
@@ -0,0 +1,25 @@
+const hapiGood = require('@hapi/good');
+
+const options = {
+    ops: {
+        interval: 1000
+    },
+    reporters: {
+        myConsoleReporter: [
+            {
+                module: '@hapi/good-squeeze',
+                name: 'Squeeze',
+                args: [{ log: '*', response: '*' }]
+            },
+            {
+                module: '@hapi/good-console'
+            },
+            'stdout'
+        ]
+    }
+};
+
+module.exports = {
+    plugin: hapiGood,
+    options: options,
+};
--- routes/hello-hapi.js
+++ routes/hello-hapi.js
@@ -3,13 +3,23 @@ const {jwtHeaderDefine} = require('../utils/router-helper');
 module.exports = [
     {
         method: 'GET',
+        path: '/',
+        options: {
+            handler: (request, h) => {
+                return {'message': 'hello hapi'};
+            },
+            auth: false,
+        },
+    },
+    {
+        method: 'GET',
         path: '/restricted',
         options: {
             handler: (request, h) => {
-                // const response = h.response({
-                //     message: '您使用有效的JWT令牌来访问限制接口！'
-                // });
-                //response.header("Authorization", request.headers.authorization);
+                const response = h.response({
+                    message: '您使用有效的JWT令牌来访问限制接口！'
+                });
+                response.header("Authorization", request.headers.authorization);
                 console.log('输出信息：', request.auth.credentials); // 控制台输出 { userId: 1}
                 return 'hello hapi';
             },
